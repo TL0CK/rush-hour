a6889d784e199837ee9768ff167cded80c02793d
Grudumpf <grudu.v@hotmail.fr>
2016-03-15 11:00:25 +0100

    Game.c V2
    Nouvelle version de game.c

--- src/game.c
+++ src/game.c
@@ -3,8 +3,6 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#define TAILLE 6
-
 struct game_s{
   int **game_board; //tableau a initialiser depuis en bas a gauche, la il est en haut a gauche, aka la case en basa
     int nb_pieces;   //droite doit etre 0 0 et la c'est 5 0
@@ -16,15 +14,36 @@ struct game_s{
 
 game new_game_hr (int nb_pieces, piece *pieces){
   game g = malloc(sizeof(struct game_s));
+ 
+  g->width = 6;
+  g->height = 6;
 
-  g->game_board = malloc(sizeof(int)*TAILLE);
+  g->game_board = malloc(sizeof(int)*g->width);
   for(int i= 0; 1<TAILLE; ++i)
-    g->game_board[i] = malloc(sizeof(int)*TAILLE);
+    g->game_board[i] = malloc(sizeof(int)*g->height);
 
   g->game_board[0][3] = 0;
-  g->piece = pieces;
   g->nb_pieces = nb_pieces;
   g->nb_moves = 0;
+  g->piece = pieces;
+
+  return g;
+}
+
+game new_game (int width, int height, int nb_pieces, piece *pieces){
+  game g = malloc(sizeof(struct game_s));
+ 
+  g->width = width;
+  g->height = height;
+
+  g->game_board = malloc(sizeof(int)*g->width);
+  for(int i= 0; 1<TAILLE; ++i)
+    g->game_board[i] = malloc(sizeof(int)*g->height);
+
+  g->game_board[0][3] = 0;
+  g->nb_pieces = nb_pieces;
+  g->nb_moves = 0;
+  g->piece = pieces;
 
   return g;
 }
@@ -55,6 +74,27 @@ int game_nb_pieces(cgame g){
 
 int game_nb_moves(cgame g){
   return g->nb_moves;
+}
+
+int game_width(cgame g){
+  return g->width;
+}
+
+int game_height(cgame g){
+  return g->height;
+}
+
+int game_square_piece (game g, int x, int y){
+  int nb_piece = 0;
+  for (int i = 0; i<x; ++i){
+    for (int j = 0; j<y; ++j){
+      if (g->game_board[i][j] != -1)
+        nb_piece += 1;
+    }
+  }
+  if (nb_piece == 0)
+    return -1;
+  return nb_piece;
 }
 
 bool game_over_hr(cgame g){
@@ -154,6 +194,3 @@ bool play_move(game g, int piece_num, dir d, int distance){
     }
   }
 }
-
-
-
