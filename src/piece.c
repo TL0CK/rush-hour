02f4b07464c4c00cdea9cfa26754614be18e8aeb
Grudumpf <grudu.v@hotmail.fr>
2016-03-17 10:57:12 +0100

    Brian is in the kitchen

--- src/piece.c
+++ src/piece.c
@@ -3,8 +3,6 @@
 
 #include "piece.h"
 
-#define TAILLE 6
-
 struct piece_s{
     int x;
     int y;
@@ -14,7 +12,8 @@ struct piece_s{
     bool move_y;
 };
 
-
+/* Create new piece for the game "Rush Hour" and allocate memory for her
+   Use x and y as position, and small and horizontal as shape*/
 piece new_piece_rh (int x, int y, bool small, bool horizontal){
   piece p = malloc(sizeof(struct piece_s));
   p -> x = x;    
@@ -46,10 +45,12 @@ piece new_piece_rh (int x, int y, bool small, bool horizontal){
   return p;
 }
 
+// Free the memory of p
 void delete_piece (piece p){
   free(p);
 }
 
+// Copy all parameter of dst and attribute all of them to src
 void copy_piece (cpiece src, piece dst){
   dst -> x = src->x;
   dst -> y = src->y;
@@ -59,6 +60,7 @@ void copy_piece (cpiece src, piece dst){
   dst -> move_y = src -> move_y;
 }
 
+// Test if the piece p can move in the direction d
 bool can_move ( piece p , dir d){
   if ( (d == UP || d == DOWN)  && p->move_x && !p->move_y)
     return false;
@@ -67,7 +69,7 @@ bool can_move ( piece p , dir d){
   return true; 
 }
 
-
+// Move the piece p in direction d and the distance from parameter 
 void move_piece (piece p, dir d, int distance){
   if(can_move(p ,d))
   switch(d){
@@ -88,18 +90,22 @@ void move_piece (piece p, dir d, int distance){
   }
 }
 
+// Return the higthest value between a and b
 int max(int a , int b){
   if (a<b)
     return b;
   return a;
 }
 
+// Return the lowest value between a and b
 int min(int a , int b){
   if (a<b)
     return a;
   return b;
 }
 
+/* Test if the piece p1 and p2 have any intersection between them
+   True if they have one, false if not*/
 bool intersect(cpiece p1 , cpiece p2){
   int maxgauche = max(get_x(p1),get_x(p2));
   int mindroit = min(get_x(p1) + get_width(p1) , get_x(p2) + get_width(p2));
@@ -111,40 +117,49 @@ bool intersect(cpiece p1 , cpiece p2){
   return false;
 }
 
+// Return the x of the piece p
 int get_x(cpiece p){
   return p->x;
 }
 
+// Return the y of the piece p
 int get_y(cpiece p){
   return p->y;
 }
 
+// Return the height of the piece p
 int get_height(cpiece p){
-  return p -> height;
+  return p->height;
 }
 
+// Return the weigth of the piece p
 int get_width(cpiece p){
- return p -> width;
+ return p->width;
 }
 
+// Test if the piece p is horizontal
 bool is_horizontal(cpiece p){
   return (p->move_x && !p->move_y);
 }
 
+// Test if the piece can move upper and downer
 bool can_move_x(cpiece p){
   return p->move_x;
 }
 
+// Test if the piece can move in right and left direction
 bool can_move_y(cpiece p){
   return p->move_y;
 }
 
+/* Create new piece for the game "Ane Rouge" and allocate memory for her
+   Use x and y as position, width and height as dimension, and small and horizontal as shape*/
 piece new_piece (int x, int y, int width, int height, bool move_x, bool move_y){
   piece p = malloc(sizeof(struct piece_s));
   p -> x = x;
   p -> y = y;
-  p -> height = height;
   p -> width = width;
+  p -> height = height;
   p -> move_x = move_x;
   p -> move_y = move_y;
   return p;
